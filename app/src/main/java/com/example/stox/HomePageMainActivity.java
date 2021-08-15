b80ecf1bcd861f1a893fbd7f51d8d56bdb471e08
anujpant08 <anujpant2@gmail.com>
2021-08-15 23:04:53 +0530

    Designing home page - Setting up Material Toolbar and Adding Search button.

--- app/src/main/java/com/example/stox/HomePageMainActivity.java
+++ app/src/main/java/com/example/stox/HomePageMainActivity.java
@@ -1,8 +1,11 @@
 package com.example.stox;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.viewpager2.widget.ViewPager2;
@@ -22,7 +25,10 @@ public class HomePageMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         com.example.stox.databinding.ActivityHomePageMainBinding binding = ActivityHomePageMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        //setSupportActionBar(binding.toolbar);
+        Toolbar mainToolbar = findViewById(R.id.material_toolbar);
+        mainToolbar.setTitle("Home");
+        mainToolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+        setSupportActionBar(mainToolbar);
         tabLayout = findViewById(R.id.tabsLayout);
         viewPager2 = findViewById(R.id.viewPager2);
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -30,6 +36,7 @@ public class HomePageMainActivity extends AppCompatActivity {
         viewPager2.setAdapter(fragmentAdapter);
         tabLayout.addTab(tabLayout.newTab().setText("Stocks"));
         tabLayout.addTab(tabLayout.newTab().setText("Index"));
+        tabLayout.setTabTextColors(Color.parseColor("#FFFFFF"), Color.parseColor("#FFFFFF"));
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -54,6 +61,12 @@ public class HomePageMainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return true;
+    }
     /*@Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home_page_main);
