ed417166bfe359c00186fdb4492796aeffa0167d
anujpant08 <anujpant2@gmail.com>
2021-08-20 22:18:34 +0530

    Implementing SharedPreferences for saving favorite stocks locally.

--- app/src/main/java/com/example/stox/StocksFragment.java
+++ app/src/main/java/com/example/stox/StocksFragment.java
@@ -1,5 +1,7 @@
 package com.example.stox;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,6 +19,7 @@ import androidx.lifecycle.ViewModelProvider;
 import com.android.volley.RequestQueue;
 import com.example.stox.databinding.StocksFragmentBinding;
 
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -28,6 +31,7 @@ public class StocksFragment extends Fragment {
     private TextView textView;
     private ListView listView;
     private List<String> stocks;
+    private Set<String> stockSet;
     private RequestQueue queue;
     private APIDataViewModel apiDataViewModel;
     private ArrayAdapter<String> arrayAdapter;
@@ -46,6 +50,16 @@ public class StocksFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         stocks = new LinkedList<>();
+        stockSet = new LinkedHashSet<>();
+        // For testing set of stocks
+
+//        stockSet.add("RIL");
+//        stockSet.add("SAIL");
+//        stockSet.add("ITC");
+
+        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("Stocks", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        stockSet = sharedPreferences.getStringSet("Stocks", stockSet);
         textView = view.findViewById(R.id.textview_first);
         listView = view.findViewById(R.id.stocks_list_view);
         arrayAdapter = new ArrayAdapter<String>(
@@ -54,20 +68,24 @@ public class StocksFragment extends Fragment {
                 stocks
         );
         listView.setAdapter(arrayAdapter);
+        arrayAdapter.addAll(stockSet);
         apiDataViewModel = new ViewModelProvider(this).get(APIDataViewModel.class);
         apiDataViewModel.getStocksList(getActivity()).observe(getViewLifecycleOwner(), new Observer<Set<String>>() {
             @Override
             public void onChanged(Set<String> updatedStocks) {
-                if(stocks != null){
+               /* if(stocks != null){
                     Log.d(TAG, "changed value: " + updatedStocks);
-                    stocks.clear();
+                    //stocks.clear();
                 }else{
                     stocks = new LinkedList<>();
-                }
+                }*/
                 Log.d(TAG, "final list : " + updatedStocks);
-                stocks.addAll(updatedStocks);
+                //stocks.addAll(updatedStocks);
+                stockSet.addAll(updatedStocks);
+                editor.putStringSet("Stocks", stockSet);
+                editor.apply();
                 arrayAdapter.clear();
-                arrayAdapter.addAll(updatedStocks);
+                arrayAdapter.addAll(stockSet);
             }
         });
     }
